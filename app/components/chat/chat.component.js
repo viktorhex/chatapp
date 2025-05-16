8aebdec365984070a0376d6a16b5a5aa4afc9522
viktorhex <christopherdillinger200@gmail.com>
2025-05-16 17:36:57 -0400

    init chat module

--- app/components/chat/chat.component.js
+++ app/components/chat/chat.component.js
@@ -1,12 +1,33 @@
 'use strict';
 
-angular.
-  module('chat').
-  component('chat', {
+angular
+  .module('chat')
+  .component('chat', {
     templateUrl: 'components/chat/chat.template.html',
-    controller: ['$location',
-      function ChatController($location) {
+    controller: ['$location', '$scope', '$timeout',
+      function ChatController($location, $scope, $timeout) {
+        this.messages = [];
 
+        this.addMessage = function(messageText) {
+          if (messageText && messageText.trim()) {
+            this.messages.push({
+              text: messageText,
+              timestamp: new Date().toLocaleTimeString()
+            });
+            this.newMessage = '';
+            $timeout(function() {
+              const chatMessages = document.querySelector('.chat-messages');
+              chatMessages.scrollTop = chatMessages.scrollHeight;
+            });
+          }
+        };
+
+        this.onKeyPress = function(event) {
+          if (event.keyCode === 13) {
+            this.addMessage(this.newMessage);
+          }
+        };
       }
     ]
   });
+  
